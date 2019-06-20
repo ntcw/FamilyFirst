f934fb24a584a96283f4f65951cd0da3c207a955
Benedikt Langer <S1810237024@students.fh-hagenberg.at>
2019-06-20 18:01:56 +0200

    first Elements

---
+++ FamilyFirst/AddMember/AddMemberSubviewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddMemberSubviewController.swift
+//  FamilyFirst
+//
+//  Created by Benedikt Langer on 20.06.19.
+//  Copyright © 2019 Niklas. All rights reserved.
+//
+
+import UIKit
+
+class AddMemberSubviewController: UITableViewController{
+    
+    
+}
---
+++ FamilyFirst/AddMember/AddMemberViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddMemberViewController.swift
+//  FamilyFirst
+//
+//  Created by Benedikt Langer on 20.06.19.
+//  Copyright © 2019 Niklas. All rights reserved.
+//
+
+import UIKit
+
+class AddMemberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
