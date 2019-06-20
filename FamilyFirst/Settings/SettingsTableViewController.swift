8870d95ac5c120601d2a17a96027cd505b789ffa
Niklas Wagner <S1810237031@students.fh-hagenberg.at>
2019-06-20 14:09:22 +0200

    Settings Implementation

---
+++ FamilyFirst/Settings/SettingsTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsTableViewController.swift
+//  FamilyFirst
+//
+//  Created by Niklas Wagner on 19.06.19.
+//  Copyright © 2019 Niklas. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func editNameButtonPressed(_ sender: UIButton) {
+        
+       
+        
+        
+    }
+    
+}
---
+++ FamilyFirst/Settings/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  FamilyFirst
+//
+//  Created by Niklas Wagner on 19.06.19.
+//  Copyright © 2019 Niklas. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingsViewController: UIViewController {
+    
+    
+    @IBOutlet var changeNameView: UIView!
+    
+    
+}
