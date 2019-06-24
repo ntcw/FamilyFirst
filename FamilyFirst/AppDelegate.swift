779b3e943ceb6f981e1ad2dbc6087e233f303330
Niklas Wagner <S1810237031@students.fh-hagenberg.at>
2019-06-24 18:49:26 +0200

    FirstLaunch finished

--- FamilyFirst/AppDelegate.swift
+++ FamilyFirst/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+          UserDefaults.standard.set(false, forKey: "firstLaunch")
      
         if !UserDefaults.standard.bool(forKey: "firstLaunch") {
             
