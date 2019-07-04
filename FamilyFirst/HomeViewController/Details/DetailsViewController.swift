a9ec3d4b87dc01af5685e8b60998e07495c23cb2
Niklas Wagner <S1810237031@students.fh-hagenberg.at>
2019-07-04 16:32:43 +0200

    Start of details

---
+++ FamilyFirst/HomeViewController/Details/DetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.swift
+//  FamilyFirst
+//
+//  Created by Niklas Wagner on 04.07.19.
+//  Copyright © 2019 Niklas. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DetailsViewController: UIViewController {
+    
+   var selectedMember: FamilyMember?
+}
--- FamilyFirst/HomeViewController/HomeViewController.swift
+++ FamilyFirst/HomeViewController/HomeViewController.swift
@@ -6,14 +6,11 @@
 //  Copyright © 2019 Niklas. All rights reserved.
 //
 
-
 import CoreData
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var members = MemberClass.allMembers
-    var selectedMember: FamilyMember?
     var memberArray: [FamilyMember] = [FamilyMember]()
     let defaults = UserDefaults.standard
     let standardPicture = UIImage(named: "smile")!
@@ -25,132 +22,132 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     var counter = 0
     let colorRedHex = "#F2522E"
     let colorBlueHex = "#36A6BF"
-    
-    @IBOutlet weak var familyLabel: UILabel!
-    
-    
-    @IBOutlet weak var tableView: UITableView!
-    
+    var selectedMember: FamilyMember?
+    let detailsSegue = "detailsSegue"
+
+    @IBOutlet var familyLabel: UILabel!
+
+    @IBOutlet var tableView: UITableView!
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         members.loadMember()
         defaults.synchronize()
-        familyLabel.font = UIFont(name:"Futura",size: 35)
+        familyLabel.font = UIFont(name: "Futura", size: 35)
         let familyname = defaults.object(forKey: "familyName") as! String
         familyLabel.text = familyname
         tableView.reloadData()
         tableView.delegate = self
         tableView.dataSource = self
-        
+        memberArray = members.getMembers()
     }
-    
-    
+
     override func viewDidLoad() {
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"multi")!)
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "multi")!)
         tableView.backgroundColor = UIColor.clear
         colorArray.append(orange)
         colorArray.append(red)
         colorArray.append(yellow)
         colorArray.append(blue)
     }
-    
-    
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return members.count()
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "MemberTableViewCell", for: indexPath) as! MemberCellTableViewCell
-        
+
         let member = members.getMember(at: indexPath.row)
         cell.Name.text = member?.name ?? ""
-        if let image = member?.picture{
-           
-            cell.memberPicture.image =  UIImage(data: image)
+        if let image = member?.picture {
+            cell.memberPicture.image = UIImage(data: image)
             cell.memberPicture.setRounded()
             cell.memberPicture.layer.borderWidth = 1
             cell.memberPicture.layer.masksToBounds = false
-           cell.memberPicture.layer.borderColor = UIColor.black.cgColor
-            cell.memberPicture.layer.cornerRadius = cell.memberPicture.frame.height/2
+            cell.memberPicture.layer.borderColor = UIColor.black.cgColor
+            cell.memberPicture.layer.cornerRadius = cell.memberPicture.frame.height / 2
             cell.memberPicture.clipsToBounds = true
-           
-            
-        }
-        else {
+        } else {
             cell.imageView?.image = standardPicture
         }
-        
+
         if counter == colorArray.count {
             counter = 0
         }
-       
+
 //        cell.layer.backgroundColor = UIColor(hexString: colorBlueHex).cgColor
 //       cell.layer.backgroundColor = colorArray[counter].cgColor
         cell.layer.backgroundColor = UIColor.clear.cgColor
         cell.accessoryType = .disclosureIndicator
         counter += 1
-        
+
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 65
     }
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
-    
-    
-    
+
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let footer = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 100))
-        
+
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 100))
-        button.backgroundColor = UIColor(red: 205/255, green: 207/255, blue: 211/255, alpha: 0.5)
+        button.backgroundColor = UIColor(red: 205 / 255, green: 207 / 255, blue: 211 / 255, alpha: 0.5)
         button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         button.setTitle("Add member", for: .normal)
-     
+
         button.setTitleColor(UIColor.black, for: .normal)
         footer.addSubview(button)
 
         return footer
     }
-    
-    @objc func buttonPressed(_ Button: UIButton){
+
+    @objc func buttonPressed(_ Button: UIButton) {
         performSegue(withIdentifier: "toAddMember", sender: self)
-        
     }
-    
+
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             memberArray = members.getMembers()
-            
+
             let memberToDelete = members.getMember(at: indexPath.row)
-            
+
             if let memberRemove = memberToDelete {
                 members.deleteMember(member: memberRemove)
                 memberArray.remove(at: indexPath.row)
-          
-                
             }
             members.loadMember()
             tableView.reloadData()
         }
     }
     
+    // Open details of abo
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        memberArray = members.getMembers()
+        selectedMember = memberArray[indexPath.row]
+        performSegue(withIdentifier: detailsSegue, sender: self)
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == detailsSegue {
+            let vc = segue.destination as! DetailsViewController
+            let realVC = vc.self
+            realVC.selectedMember = selectedMember
+        }
+    }
     
 }
 
-
-
 extension UIImageView {
-    
     func setRounded() {
-        self.layer.cornerRadius = (self.frame.width / 2)
-        self.layer.masksToBounds = true
+        layer.cornerRadius = (frame.width / 2)
+        layer.masksToBounds = true
     }
 }
